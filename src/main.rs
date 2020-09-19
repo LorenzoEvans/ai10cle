b9f93dbb290f40b4649542314dccaccfd2646354
LorenzoEvans <lorenzo.evans94@gmail.com>
2020-09-19 13:52:22 -0700

    Minor API tweaks

--- src/main.rs
+++ src/main.rs
@@ -21,9 +21,21 @@ use actix::prelude::*;
 
 // web:Data uses Arc internally, thus to avoid double Arc, we should create our 
 // Data before registering it, using App::app_data()
+
+struct Article {
+    title: String,
+    content: String
+}
+struct User {
+    username: String,
+    password: String,
+    liked_articles: Vec<Article>
+}
 struct AppState {
     // THIS MIGHT BE WHERE WE WANT TO USE OUR LOGIC GATES TO IMPLEMENT THE LS8.
     app_name: String,
+    articles: Vec<Article>
+    users: Vec<Users>
 
 }
 
@@ -41,20 +53,20 @@ impl Actor for MyActor {
 // that accepts parameters, that can be extracted from a request,
 // for example, impl FromRequest, and returns a type that can
 // be cast to an HttpResponse, impl Responder.
-async fn index() -> impl Responder {
-    HttpResponse::Ok().body("Hello World")
-}
 
-async fn index_2() -> impl Responder {
-    HttpResponse::Ok().body("Hello again!")
-}
 
-async fn index_3(data: web::Data<AppState>) -> String {
+async home(data: web::Data<AppState>) -> String {
     let app_name = &data.app_name;
-    format!("Hello {}!", app_name)
+    format!("Welcome to {}!", app_name)
 }
 
+async fn articles() -> impl Responder {
+    HttpResponse::Ok().body("Here are the articles!")
+}
 
+async fn profile() -> impl Responder {
+    HttpResponse::Ok().body("Here is your profile")
+}
 // Next we have an instance of App from actix-web,
 // and we register the request handlers, with the applications,
 // route on a path, with a particular Http method.
@@ -63,13 +75,13 @@ async fn index_3(data: web::Data<AppState>) -> String {
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
     let state = web::Data::new(AppState {
-        app_name: String::from("Rust2Webtris")
+        app_name: String::from("AI10cle")
     });
     HttpServer::new(|| {
         App::new()
-            .route("/rust2webtris", web::get().to(index_3))
-            .route("/", web::get().to(index))
-            .route("/again", web::get().to(index_2))
+            .route("/", web::get().to(home))
+            .route("/articles", web::get().to(index))
+            .route("/profile", web::get().to(index_2))
     })
     .bind("127.0.0.1:8088")?
     .run()
